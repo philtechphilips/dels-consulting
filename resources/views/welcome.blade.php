125c95c63949d64edd45f66633daab1d446bbcbb
Isola Pelumi <69679124+philtechphilips@users.noreply.github.com>
2024-03-09 16:18:27 +0100

    feat: section 2 completed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,6 +81,36 @@
         </div>
         <div class="swiper-pagination"></div>
     </section>
+
+
+    <section class="w-full py-28 flex md:flex-row flex-col relative">
+        <div class="bg-[#CDDDF5] md:rounded-r-full px-5 md:pl-16 md:pr-28 py-28 md:w-[750px] flex flex-col md:justify-start md:items-start justify-center items-center">
+            <h1 class="md:text-4xl text-3xl font-bold text-[#010101] md:text-left text-center">Welcome To <span class="text-[#0454CB]">DELS CONSULTING</span></h1>
+            <p class="mt-4 text-lg font-medium  md:text-left text-center">At <span class="text-[#0454CB]">DELS Consulting</span>, we have a professional team to help you grow your business. Want to know how? Click on the link below and learn more from us how we can help you!</p>
+            <a href="" class="text-center bg-[#0454CB] rounded-full px-12 text-sm font-medium mt-4 py-2 w-fit text-white flex items center">Learn More <i class="ri-arrow-right-s-line mt-[1px]"></i></a>
+        </div>
+
+        <div class="md:flex gap-4 items-center -ml-28 hidden">
+            <div>
+                <img src="{{ asset("images/business-owner-working-their-strategy 1.png")}}" width="350">
+            </div>
+
+            <div class="flex flex-col gap-4">
+                <img src="{{ asset("images/business-owner-working-their-strategy (2) 1.png")}}"  class="z-10">
+                <img src="{{ asset("images/business-owner-working-their-strategy (3) 1.png")}}" class="z-10">
+            </div>
+        </div>
+
+        <div class=" absolute w-[250px] top-0 z-0 right-0 hidden md:flex">
+            <img src="{{ asset("images/54545454 3.png") }}" class="z-0" width="250" >
+        </div>
+        <div class=" absolute w-[250px] top-24 z-0 -right-[50px] hidden md:flex">
+            <img src="{{ asset("images/Vector.png") }}" class="z-0" width="30" >
+        </div>
+        <div class=" absolute w-[250px] bottom-40 z-0 -right-[104px] hidden md:flex">
+            <img src="{{ asset("images/Vector.png") }}" class="z-0" width="20" >
+        </div>
+    </section>
 @endsection
 
 
