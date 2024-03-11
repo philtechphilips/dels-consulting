efdd81360cf2f1f6d7403a06e99b5f0c729330cd
Isola Pelumi <69679124+philtechphilips@users.noreply.github.com>
2024-03-11 23:33:43 +0100

    feat: testimonal done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -385,7 +385,7 @@
         </div>
     </section>
 
-    <section class="md:px-24 px-5 md:pr-20 py-40" style="background-image: url('{{ asset('images/map.png') }}'); background-repeat: no-repeat; background-position: center; background-size: 700px;">
+    <section class="md:px-24 px-5 md:pr-20 md:py-40" style="background-image: url('{{ asset('images/map.png') }}'); background-repeat: no-repeat; background-position: center; background-size: 700px;">
         <div class="md:w-1/2">
             <h1 class="text-4xl font-bold">Contact Us</h1>
             <p class="text-[#4F4F4F] text-lg mt-2 md:pr-28">You can contact us through the form below and communication channels</p>
@@ -424,12 +424,12 @@
         </div>
     </section>
 
-    <section class="md:pl-24 py-28" style="background-image: url('{{ asset('images/circle4.png') }}'); background-repeat: no-repeat; background-position: center left; background-size: 700px;">
-        <div class="px-28 flex items-end flex-col mb-8">
-            <h1 class="font-bold text-4xl mt-2">What Our Client Say</h1>
-            <p class="w-96 text-base mt-2 text-[#474747]">In this section, a brief description of the blog content and what can be useful is written</p>
+    <section class="md:pl-24 pl-0 px-5 md:px-0 py-28" style="background-image: url('{{ asset('images/circle4.png') }}'); background-repeat: no-repeat; background-position: center left; background-size: 700px;">
+        <div class="md:px-28 px-5 flex items-end flex-col md:mb-8">
+            <h1 class="font-bold text-4xl mt-2 text-center md:text-left">What Our Client Say</h1>
+            <p class="md:w-96 text-base mt-2 text-[#474747] text-center md:text-left">In this section, a brief description of the blog content and what can be useful is written</p>
         </div>
-        <div class="w-full h-screen flex items-center px-16 gap-10" style="background-image: url('{{ asset('images/quoted.png') }}'); background-repeat: no-repeat; background-position: center left; background-size: cover;">
+        <div class="w-full hidden h-screen md:flex items-center md:px-16 gap-10" style="background-image: url('{{ asset('images/quoted.png') }}'); background-repeat: no-repeat; background-position: center left; background-size: cover;">
             <div class="flex gap-5 p-0 items-center w-1/2 ">
                 <img src="{{ asset("images/woman.png") }}">
             </div>
@@ -437,6 +437,17 @@
             <div class="flex flex-col gap-4 w-1/2">
                 <h1 class="font-bold text-xl">Iranian Health Company CEO </h1>
                 <p class="">Before they’d even arrived, all three of the features writers for T’s Travel issue immersed themselves in the place to which they were going with a number of books, television shows, songs and films. After all, the assignment — to return to ..</p>
+            </div>
+        </div>
+
+        <div class="w-full md:hidden h-screen flex items-center md:px-16 md:gap-10">
+            <div class="hidden gap-5 p-0 items-center w-1/2 ">
+                <img src="{{ asset("images/woman.png") }}">
+            </div>
+
+            <div class="flex flex-col gap-4 md:w-1/2 px-5 -mt-80 md:mt-0">
+                <h1 class="font-bold text-xl text-center md:text-left">Iranian Health Company CEO </h1>
+                <p class="text-center md:text-left">Before they’d even arrived, all three of the features writers for T’s Travel issue immersed themselves in the place to which they were going with a number of books, television shows, songs and films. After all, the assignment — to return to ..</p>
             </div>
         </div>
     </section>
