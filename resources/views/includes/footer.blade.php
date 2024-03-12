a001a4926c726634048342302279359800cd9065
Isola Pelumi <69679124+philtechphilips@users.noreply.github.com>
2024-03-12 09:01:31 +0100

    feat: footer done

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,9 +1,38 @@
 <footer class="w-full">
-<div class="w-full bg-white px-12 py-12 flex items-center justify-between">
+    <div class="w-full bg-white md:px-12 px-5 py-12 flex flex-col gap-5 md:gap-0 md:flex-row items-start justify-between">
+        <img src="{{ asset("images/DELS Main Logo.png") }}" class="w-28">
 
-</div>
+        <div class="flex flex-col gap-3 px-1">
+            <h1 class="text-lg font-semibold text-[#0454CB]">Services</h1>
+            <h1 class="">Design & Development</h1>
+            <h1 class="">SEO</h1>
+            <h1 class="">Advertising</h1>
+            <h1 class="">Social Management</h1>
+        </div>
+
+
+        <div class="flex flex-col gap-3 px-1">
+            <h1 class="text-lg font-semibold text-[#0454CB]">Packages</h1>
+            <h1 class="">Design</h1>
+            <h1 class="">Development</h1>
+            <h1 class="">SEO</h1>
+            <h1 class="">Social</h1>
+        </div>
+
+        <div class="flex flex-col gap-3 px-1">
+            <h1 class="text-lg font-semibold text-[#0454CB]">Activity</h1>
+            <h1 class="">Customer Services</h1>
+            <h1 class="">Features</h1>
+        </div>
+
+        <div class="flex flex-col gap-3 px-1">
+            <h1 class="text-lg font-semibold text-[#0454CB]">Services</h1>
+        </div>
+       
+
+    </div>
     <div class="w-full flex items-center justify-between md:px-24 px-5 py-4 bg-[#F5F5F7]">
-        <p class="text-xs text-[#6D6D6D] text-center">© {{ date("Y") }} Dells. All rights reserved.</p>
+        <p class="text-xs text-[#6D6D6D] text-center">© {{ date('Y') }} Dells. All rights reserved.</p>
         <p class="text-xs text-[#6D6D6D] text-center">Terms and Conditions</p>
         <p class="text-xs text-[#6D6D6D] text-center">Privacy Policy</p>
     </div>
